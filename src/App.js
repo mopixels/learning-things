1bd210ee7716a01785da6e33eb7f974564c867f1
Modestas Rimkus <modestas.rimkus@oxylabs.io>
2021-03-10 12:34:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,25 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import CharacterPage from "./pages/character";
-import HomePage from "./pages/homePage";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [characters, setCharacters] = useState("");
-
-  useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setCharacters(result);
-          setIsLoaded(true);
-        },
-        (error) => {
-          setError(error);
-          setIsLoaded(true);
-        }
-      );
-  }, []);
-
-  if (error) {
-    return <div>Error: sorry something went wrong please try again later</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomePage data={characters} />
-          </Route>
-
-          <Route path="/:character">
-            <CharacterPage data={characters} />
-          </Route>
-        </Switch>
-      </Router>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/utils/formatName.js
+++
@@ -1,3 +0,0 @@
-export const formatName = (name) => {
-  return name.replace(/\s/g, "-").toLowerCase();
-};

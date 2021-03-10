ff127ecae536c314d9cce5d1ab03ef59bf666af6
Modestas Rimkus <modestas.rimkus@oxylabs.io>
2021-03-10 12:25:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CharacterPage from "./pages/character";
+import HomePage from "./pages/homePage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [characters, setCharacters] = useState("");
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setCharacters(result);
+          setIsLoaded(true);
+        },
+        (error) => {
+          setError(error);
+          setIsLoaded(true);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: sorry something went wrong please try again later</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage data={characters} />
+          </Route>
+
+          <Route path="/:character">
+            <CharacterPage data={characters} />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
